ab36a81f58ecd572a5c0943d4faa1b474eaba557
Proxypepe <sasha_golovin_95@bk.ru>
2023-01-23 15:15:13 +0300

    added gen secrets for passwords

--- start.sh
+++ start.sh
@@ -16,16 +16,19 @@ docker service create --name registry --publish published=5000,target=5000 regis
 db_password=$(docker secret ls | grep db_password)
 db_root_password=$(docker secret ls | grep db_root_password)
 
-if [[ -z "$db_password" && -z "$db_root_password" ]]; then
-  echo "Creating new secrets for db passwords"
+if [[ -z "$db_password" ]]; then
+  echo "Creating a new secret for db password"
   openssl rand -base64 9 | docker secret create db_password -
-  openssl rand -base64 9 | docker secret create db_root_password -
 else
-  echo "Secrets ok"
+  echo "Db secret is ok"
 fi
 
-openssl rand -base64 9 | docker secret create db_password -
-openssl rand -base64 9 | docker secret create db_root_password -
+if [[ -z "$db_root_password" ]]; then
+  echo "Creating a new secret for db  root password"
+  openssl rand -base64 9 | docker secret create db_root_password -
+else
+  echo "Db root secret is ok"
+fi
 
 docker build -t 127.0.0.1:5000/flask-app:1 src/
 docker push 127.0.0.1:5000/flask-app:1
